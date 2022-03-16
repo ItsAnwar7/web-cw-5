0d749faf581c056406a8dace8bffc9ace1ed6077
anwar <ALFADHLIANWAR7@GMAIL.COM>
2022-03-16 19:35:40 +0300

    cw2

--- javaS.js
+++ javaS.js
@@ -3,3 +3,8 @@ console.log(16)
 console.log(`انا في الصف الحادي عشر`)
 console.log('%cانا في الصف الحادي عشر', 'color: green; background: yellow; font-size: 30px');
 //hello
+
+let budget= 20
+console.log(budget>30 && budget<80)
+budget += 30;
+console.log(budget>30 && budget<80)
