4f8a44522cc1acd4af90481d5498cd3c38406cc3
anwar <ALFADHLIANWAR7@GMAIL.COM>
2022-03-16 20:42:17 +0300

    cw3

--- javaS.js
+++ javaS.js
@@ -8,3 +8,22 @@ let budget= 20
 console.log(budget>30 && budget<80)
 budget += 30;
 console.log(budget>30 && budget<80)
+
+let grade = prompt (`ادخل درجتك`)
+if (grade >=90 ){
+    console.log(`لقد حصلت على امتياز`)
+}
+else if (grade >=80){
+    console.log(` لقد حصلت على جيد جدا `)
+}
+else if (grade >=70){
+    
+        console.log(` لقد حصلت على جيد جدا `)
+ }
+ else if (grade>=60){
+     console.log( "لقد حصلت على مقبول😕")
+ }
+ else if (grade>= 50){
+     console.log("راسب💔")
+ }
+
